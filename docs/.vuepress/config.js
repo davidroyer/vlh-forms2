415cc3c3399798b0954099217d273403e7ee1c9e
David Royer <droyer01@gmail.com>
2018-11-29 16:09:03 -0500

    hoping fixes svg issue

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -54,9 +54,13 @@ module.exports = {
     }
   },
   chainWebpack: config => {
-    const svgRule = config.module.rule("svg");
-    svgRule.uses.clear();
-    svgRule.use("vue-svg-loader").loader("vue-svg-loader");
+    config.module.rules.delete('svg')
+    config.module
+      .rule('svg')
+        .test(/\.svg$/)
+        .use('vue-svg-loader')
+          .loader('vue-svg-loader')
+          .end()    
   }  
 }
 
