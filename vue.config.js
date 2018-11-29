1c5c02e7b37a98ac2c3d75a4225f981f3f86be0d
David Royer <droyer01@gmail.com>
2018-11-29 03:27:47 -0500

    chore(vue.config.js): A little tidying up

--- vue.config.js
+++ vue.config.js
@@ -5,15 +5,7 @@ module.exports = {
   },
   chainWebpack: config => {
     const svgRule = config.module.rule("svg");
-
     svgRule.uses.clear();
     svgRule.use("vue-svg-loader").loader("vue-svg-loader");
   }
-  // configureWebpack: {
-  //   output: {
-  //     library: "VlhForms",
-  //     filename: "vlh-forms.js",
-  //     libraryExport: "default"
-  //   }
-  // }
 };
